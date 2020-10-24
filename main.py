df376397a82995cba30b15ceb47bd23c526b721b
benihayashi <59077297+benihayashi@users.noreply.github.com>
2020-10-24 16:06:54 +0800

    preprocessing

--- main.py
+++ main.py
@@ -1,19 +1,43 @@
 #%%
 from tensorflow import keras
-from keras import layers
 import tensorflow as tf
 import PIL as pil
+from PIL import ImageOps
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
 # %%
-def img_to_array(img_path, resize = False, width = 256, height = 256) :
-    img = pil.Image.open("7001-21.jpg")
-    img = img.rotate(270)
-    img = np.array(img,np.int32) / 255
-    if(resize == True) : img = tf.image.resize(img,(width,height))
+#data preprocessing (remove the suroundding noises and emphasize the crack)
+def img_to_array(img_path, augmented=False) :
+    width = 128
+    height = 128
+    img = pil.Image.open(img_path)
+    img = (np.array(img,np.int32) + 120) / 255
+    img = tf.image.adjust_contrast(img,3)
+    img = tf.image.rgb_to_grayscale(img)
+    img = tf.image.resize(img,(width,height))
+    if(augmented) : 
+        arr = np.empty((0,width,height,1),np.int32)
+        arr = np.append(arr,img)
+        for i in range(3) :
+            img = tf.image.rot90(img)
+            arr = np.append(arr,img)
+        arr = arr.reshape((4,width,height,1))
+        return arr
     return img
-# %%
 
-model = keras.models.Sequential()
+# %%
+img = img_to_array("7004-142.jpg",augmented=True)
+for i in range(img.shape[0]) :
+    plt.imshow(img[i])
+    plt.show()
+
+#%%
+#data preparation loop
+
+# %%
+#build a model
+model = keras.models.Sequential([
+    
+])

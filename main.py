4ef70b5491bdcd2c72529df3943c5030d94e0a8f
benihayashi <59077297+benihayashi@users.noreply.github.com>
2020-10-24 16:40:12 +0800

    process loop done

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from PIL import ImageOps
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import os
 
 # %%
 #data preprocessing (remove the suroundding noises and emphasize the crack)
@@ -15,7 +16,7 @@ def img_to_array(img_path, augmented=False) :
     img = pil.Image.open(img_path)
     img = (np.array(img,np.int32) + 120) / 255
     img = tf.image.adjust_contrast(img,3)
-    img = tf.image.rgb_to_grayscale(img)
+    #img = tf.image.rgb_to_grayscale(img)
     img = tf.image.resize(img,(width,height))
     if(augmented) : 
         arr = np.empty((0,width,height,1),np.int32)
@@ -23,7 +24,7 @@ def img_to_array(img_path, augmented=False) :
         for i in range(3) :
             img = tf.image.rot90(img)
             arr = np.append(arr,img)
-        arr = arr.reshape((4,width,height,1))
+        arr = arr.reshape((4,width,height,3))
         return arr
     return img
 
@@ -35,7 +36,18 @@ for i in range(img.shape[0]) :
 
 #%%
 #data preparation loop
+raw_data_path = "D:\\Python projects\\hackathon dataset\\Decks\\c-test";
+processed_data_path = "D:\\Python projects\\hackathon dataset\\Processed\\Cracked"
 
+for file in os.listdir(raw_data_path):
+    pathname = raw_data_path + "\\" + file
+    img = img_to_array(pathname, augmented=True)
+    for i in range(img.shape[0]) :
+        name = file.split(".")[0]
+        extension = pathname.split(".")[1]
+        full_path = processed_data_path + name + "-" + str(i) + extension
+        plt.imshow(img[i])
+        plt.savefig(full_path)
 # %%
 #build a model
 model = keras.models.Sequential([

1b560b1b15c3ad40f50f60d727d3fc60d251b129
benihayashi <59077297+benihayashi@users.noreply.github.com>
2020-10-25 20:15:23 +0800

    another test

--- demo/wsgi.py
+++ demo/wsgi.py
@@ -12,7 +12,7 @@ import os
 #from whitenoise.django import DjangoWhiteNoise
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'demo.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
 
 application = get_wsgi_application()
 #application = DjangoWhiteNoise(application)
